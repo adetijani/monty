ed8395c023fca2e118e8f5cc05e462ea1b5ca94e
Debo <tijanidebo@gmail.com>
2022-10-09 11:46:49 +0100

    Monty

--- monty.h
+++ monty.h
@@ -1,13 +1,14 @@
-#ifndef MONTY
-#define MONTY
+#ifndef MONTY_H
+#define MONTY_H
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <ctype.h>
+#include <string.h>
+
+#define STACK 0
+#define QUEUE 1
+#define DELIM " \t"
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -16,7 +17,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -26,34 +27,12 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct globals - global structure to use in the functions
- * @lifo: is stack or queue
- * @cont: current line
- * @arg: second parameter inside the current line
- * @head: doubly linked list
- * @fd: file descriptor
- * @buffer: input text
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
-typedef struct globals
-{
-	int lifo;
-	unsigned int cont;
-	char  *arg;
-	stack_t *head;
-	FILE *fd;
-	char *buffer;
-} global_t;
-
-/**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
@@ -61,43 +40,75 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern global_t vglo;
+/**
+ * struct global_s - a global struct to hold info regarding the entire project
+ * @head: head of the stack
+ * @tail: tail of the stack
+ * @mode: mode of the data LIFO or FIFO
+ * @quit: variable for exit
+ * @arg: argument if any
+ */
+typedef struct global_s
+{
+	stack_t *head;
+	stack_t *tail;
+	int mode;
+	int quit;
+	char *arg;
+} global_t;
 
-/* opcode_instructuions*/
-void _push(stack_t **stack, unsigned int line_number);
-void _pall(stack_t **stack, unsigned int line_number);
-void _pint(stack_t **doubly, unsigned int cline);
-void _pop(stack_t **doubly, unsigned int cline);
-void _swap(stack_t **doubly, unsigned int cline);
-void _queue(stack_t **doubly, unsigned int cline);
-void _stack(stack_t **doubly, unsigned int cline);
-void _add(stack_t **doubly, unsigned int cline);
-void _nop(stack_t **doubly, unsigned int cline);
-void _sub(stack_t **doubly, unsigned int cline);
-void _div(stack_t **doubly, unsigned int cline);
-void _mul(stack_t **doubly, unsigned int cline);
-void _mod(stack_t **doubly, unsigned int cline);
-void _pchar(stack_t **doubly, unsigned int cline);
-void _pstr(stack_t **doubly, unsigned int cline);
-void _rotl(stack_t **doubly, unsigned int cline);
-void _rotr(stack_t **doubly, unsigned int cline);
+/* ========== Globals =========== */
+extern global_t global;
 
-/*get function*/
-void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
+/* =========== Handlers ============ */
+void push_handler(stack_t **, unsigned int);
+void pall_handler(stack_t **, unsigned int);
+void pop_handler(stack_t **, unsigned int);
+void swap_handler(stack_t **, unsigned int);
+void add_handler(stack_t **, unsigned int);
+void nop_handler(stack_t **, unsigned int);
+void sub_handler(stack_t **, unsigned int);
+void mul_handler(stack_t **, unsigned int);
+void div_handler(stack_t **, unsigned int);
+void mod_handler(stack_t **, unsigned int);
+void pint_handler(stack_t **, unsigned int);
+void rotl_handler(stack_t **, unsigned int);
+void pstr_handler(stack_t **, unsigned int);
+void pchr_handler(stack_t **, unsigned int);
+void rotr_handler(stack_t **, unsigned int);
+instruction_t _get_handler(const char *);
 
-/*imported functions*/
-int _sch(char *s, char c);
-char *_strtoky(char *s, char *d);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int _strcmp(char *s1, char *s2);
+/* =========== Stack Ops =========== */
+void push(int);
+int pop(void);
+void print_stack(void);
 
-/* doubly linked list functions */
-stack_t *add_dnodeint_end(stack_t **head, const int n);
-stack_t *add_dnodeint(stack_t **head, const int n);
-void free_dlistint(stack_t *head);
+/* =========== Queue Ops =========== */
+int dequeue(void);
+void print_queue(void);
+void enqueue(int);
 
-/* main */
-void free_vglo(void);
+/* =========== custome Ops =========== */
+void swap(void);
+void calculator(char, char *,  int);
 
-#endif
+/* ========== File handlers ========= */
+char *read_file(const char *);
+
+/* ========== Parse functions ========= */
+void parse_instructions(char **);
+int simple_opcodes(char **);
+
+/* ========== Helpers ========== */
+int is_delim(char, char *);
+char **strtow(char *, char *);
+int _isdigit(int);
+int is_number(char *);
+int arrlen(char **);
+void truncate_on_empty_line(char *);
+
+/* ========== Memory functions ========== */
+void clear_memory(void);
+void free_tokenized(char **);
+
+#endif /* MONTY_H */
